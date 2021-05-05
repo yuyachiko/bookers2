37a7bf23d4f8e5d38b1e362f6fb30b22f31dad8a
yuyachiko <yuyachiko821@gmail.com>
2021-05-05 10:53:35 +0000

    いいね機能の修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,16 +1,10 @@
 class Favorite < ApplicationRecord
 
-  MAX_FAVORITE_COUNT=1
 
   belongs_to :user
   belongs_to :book
 
   validates_uniqueness_of :book_id, scope: :user_id
-  validate :favorites_count_must_be_within_limit
+  validates_uniqueness_of :id, scope: :user_id
 
-  private
-
-   def favorites_count_must_be_within_limit
-      errors.add(:favorite, "favorites count limit: #{MAX_FAVORITE_COUNT}") if user.favorites.count >= MAX_FAVORITE_COUNT
-   end
 end
